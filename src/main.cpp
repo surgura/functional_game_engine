fa5e0f8df39013032422c64fd923284cbcfc136c
Aart Stuurman <aartstuurman@hotmail.com>
2019-05-10 18:33:26 +0200

    Filled in stub functions for tables

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <cstdint>
+#include <iostream>
+#include <string>
 #include "table.hpp"
+#include <thread>
 
 struct vector
 {
@@ -7,21 +10,61 @@ struct vector
      std::int32_t y;
 };
 
-tables::table<vector> position_table;
-tables::table<vector> velocity_table;
-using physics_table_t = tables::table<tables::row_reference<vector>, tables::row_reference<vector>>;
-physics_table_t physics_table;
+using physics_table_t = tables::table_t<tables::row_reference_t<vector>, tables::row_reference_t<vector>>;
 
-void physics_step(physics_table_t& physics)
+void physics_step(physics_table_t& physics_table)
 {
-     for (auto& row : physics)
+     for (auto& row : physics_table)
      {
-          std::get<0>(*std::get<0>(row)).x += std::get<0>(*std::get<1>(row)).x;
-          std::get<0>(*std::get<0>(row)).y += std::get<0>(*std::get<1>(row)).y;
+          vector& position = std::get<0>(*std::get<0>(*row));
+          vector& velocity = std::get<0>(*std::get<1>(*row));
+
+          position.x += velocity.x;
+          position.y += velocity.y;
+     }
+}
+
+struct texture
+{
+     char tex;
+};
+
+using draw_table_t = tables::table_t<tables::row_reference_t<vector>, tables::row_reference_t<texture>>;
+
+void draw(draw_table_t& draw_table)
+{
+     for (auto& row : draw_table)
+     {
+          vector& position = std::get<0>(*std::get<0>(*row));
+          texture& tex = std::get<0>(*std::get<1>(*row));
+          // TODO print
+          std::cout << "Drawing: " << tex.tex << " @ " << position.x << " " << position.y << std::endl;
      }
 }
 
 int main()
 {
+     tables::table_t<vector> position_table;
+     tables::table_t<vector> velocity_table;
+     tables::table_t<texture> texture_table;
+     physics_table_t physics_table;
+     draw_table_t draw_table;
+
+     // create the player
+     auto position = position_table.add_row({{1, 1}});
+     auto velocity = velocity_table.add_row({{1, 0}});
+     auto texture = texture_table.add_row({{'P'}});
+
+     physics_table.add_row({position, velocity});
+     draw_table.add_row({position, texture});
+
+     while(true)
+     {
+          //std::string input;
+          //std::cin >> input;
+          physics_step(physics_table);
+          draw(draw_table);
+          std::this_thread::sleep_for(std::chrono::milliseconds(500));
+     }
      return 0;
 }
--- src/table.hpp
+++ src/table.hpp
@@ -2,36 +2,70 @@
 
 #include <cstdint>
 #include <tuple>
+#include <list>
 
 namespace tables
 {
 
 template <typename... columns>
-using row = std::tuple<columns...>;
+using row_t = std::tuple<columns...>;
 
 template <typename... columns>
-class row_reference
+class row_reference_t
 {
+    row_t<columns...>& row;  
 public:
-    row<columns...>& operator*();
+    row_reference_t(row_t<columns...>& row) :
+        row(row)
+    {}
+    row_t<columns...>& operator*() { return row; }
 };
 
 template <typename... columns>
-class table
+class table_t
 {
+    std::list<row_t<columns...>> rows;
 public:
     class iterator
     {
+        typename std::list<row_t<columns...>>::iterator iter;
     public:
-        row<columns...>& operator*();
-        bool operator !=(iterator const& other);
-        void operator ++();
+        iterator(typename std::list<row_t<columns...>>::iterator iter) :
+            iter(iter)
+        {}
+
+        row_reference_t<columns...> operator*()
+        {
+            return { *iter };
+        }
+        
+        bool operator !=(iterator const& other)
+        {
+            return iter != other.iter;
+        }
+
+        void operator ++()
+        {
+            iter++;
+        }
     };
 
-    row_reference<columns...> add_row(row<columns...>&& new_row);
+    row_reference_t<columns...> add_row(row_t<columns...>&& new_row)
+    {
+        rows.push_back(new_row);
+        return { rows.back() };
+    }
     
-    iterator begin();
-    iterator end();
+    iterator begin()
+    {
+        return { rows.begin() };
+    }
+
+    iterator end()
+    {
+        return { rows.end() };
+    }
+
 };
 
 }
